36a846035e1d770d5d5cd6e93ec76fcc7bfb10d4
ferayala250104 <110630323+ferayala250104@users.noreply.github.com>
2022-09-03 01:01:36 -0300

    E-Commerce

--- js/categories.js
+++ js/categories.js
@@ -6,6 +6,7 @@ let currentSortCriteria = undefined;
 let minCount = undefined;
 let maxCount = undefined;
 
+
 function sortCategories(criteria, array){
     let result = [];
     if (criteria === ORDER_ASC_BY_NAME)
@@ -100,7 +101,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         sortAndShowCategories(ORDER_ASC_BY_NAME);
     });
 
-    document.getElementById("sortDesc").addEventListener("click", function(){
+    document.getElementById("sortDesc").addEventListener("click", function(){Z
         sortAndShowCategories(ORDER_DESC_BY_NAME);
     });
 
