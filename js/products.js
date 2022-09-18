7e0f9cf51c5b7d84470b33c18b854e975933bb42
ferayala250104 <110630323+ferayala250104@users.noreply.github.com>
2022-09-18 02:34:41 -0300

    E-Commerce

--- js/products.js
+++ js/products.js
@@ -2,14 +2,14 @@ let listaProd = [];
 let max = undefined;
 let min = undefined;
 let getCatID = localStorage.getItem("catID");
-const LISTADO_PRODUCTOS_URL = `${PRODUCTS_URL}${getCatID}${EXT_TYPE}`;
+const LISTADO_PRODUCTOS_URL = `${PRODUCTS_URL}${getCatID}${EXT_TYPE}`; //Nuevo link para visualizar todas las listas de productos
 
-
+//Guardar ID del producto y redirigir a su información
 function setProdID (idProd){
         localStorage.setItem("prodID", idProd)
         window.location = "product-info.html";
 }
-
+    
 function mostrarListaProductos(listaProd){
 
     let htmlContentToAppend = "";
@@ -21,6 +21,7 @@ function mostrarListaProductos(listaProd){
         console.log(typeof product.cost)
         console.log(typeof min)
 
+ //Condicionales para el filtro por precio       
       if ((min == undefined && max == undefined) || 
           (product.cost >= min && product.cost <= max) || 
           (product.cost >= min && max == undefined) || 
@@ -28,7 +29,7 @@ function mostrarListaProductos(listaProd){
 
           htmlContentToAppend += `
 
-          <div (${product.id})" class="list-group-item list-group-item-action cursor-active">
+          <div onclick="setProdID(${product.id})" class="list-group-item list-group-item-action cursor-active">
           <div class="row">
               <div class="col-3">
                   <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
@@ -36,7 +37,7 @@ function mostrarListaProductos(listaProd){
               <div class="col">
                   <div class="d-flex w-100 justify-content-between">
                       <h4 class="mb-1">${product.name}</h4>
-                      <small class="text-muted">${product.soldCount} artículos</small>
+                      <small class="text-muted">${product.soldCount} disponibles</small>
                   </div>
                   <p class="mb-1">${product.cost}  ${product.currency}</p>
               </div>
@@ -49,14 +50,18 @@ function mostrarListaProductos(listaProd){
         
     } 
        document.getElementById("list-products").innerHTML = htmlContentToAppend;
-       console.log(listaProd);
-    }
+        
+    
+    
+}
+    
 
 
     
     
 
     document.addEventListener("DOMContentLoaded", function(e) {
+
         getJSONData(LISTADO_PRODUCTOS_URL).then(function(result){
             if (result.status == "ok"){
                 listaProd = result.data.products;
@@ -69,6 +74,8 @@ function mostrarListaProductos(listaProd){
             
         });
 
+
+//Filtro que ordena de forma descendente los artículos por precio
         document.getElementById("mayMen").addEventListener("click", function(){
             listaProd.sort(function(a, b){
                 return parseInt(b.cost) - parseInt(a.cost);
@@ -76,13 +83,16 @@ function mostrarListaProductos(listaProd){
             mostrarListaProductos(listaProd);
         });
 
+//Filtro que ordena de forma ascendente los artículos por precio
         document.getElementById("menMay").addEventListener("click", function(){
             listaProd.sort(function(a,b){
                 return parseInt(a.cost) - (b.cost);
             });
             mostrarListaProductos(listaProd);
         });
+    });
 
+//Filtro que ordena de forma descendente los artículos por relevancia (Cantidad de vendidos)    
         document.getElementById("relArt").addEventListener("click", function(){
             listaProd.sort(function(a, b){
                 return parseInt(b.soldCount) - parseInt(a.soldCount);
@@ -90,6 +100,7 @@ function mostrarListaProductos(listaProd){
             mostrarListaProductos(listaProd);
         });
 
+//Botón que quita los filtros
         document.getElementById("clear-filter").addEventListener("click", function(){
             document.getElementById("filter-min").value = "";
             document.getElementById("filter-max").value = "";
@@ -100,6 +111,7 @@ function mostrarListaProductos(listaProd){
             mostrarListaProductos(listaProd);
         });
 
+//Filtro para poner un mínimo y un máximo de precio
         document.getElementById("filter-count").addEventListener("click", function(){
             
             if (document.getElementById("filter-min").value != ""){
@@ -114,8 +126,7 @@ function mostrarListaProductos(listaProd){
                 max = undefined;
             }
     
-            
-            mostrarListaProductos(listaProd);
-        });
         
-    }); 
+            mostrarListaProductos(listaProd);
+ });
+  
