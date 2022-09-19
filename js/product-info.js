643e15efd34553ee7dd64c3aad150adf7ec4d84e
ferayala250104 <110630323+ferayala250104@users.noreply.github.com>
2022-09-19 00:21:27 -0300

    E-Commerce

--- js/product-info.js
+++ js/product-info.js
@@ -6,47 +6,50 @@ const PRODUCTS_COMMENTS = `${PRODUCT_INFO_COMMENTS_URL}${getProdID}${EXT_TYPE}`;
 
 
 
-
-function mostrarInfoProd(productInfo){
+function mostrarInfoProd(productInfo) {
 
     let infoProductos = "";
-    for(let i = 0; i < productInfo.length; i++){
-        let infoProducto = productInfo[i];
-        
+    let infoProducto = productInfo;
 
-        infoProductos += ` 
-        <div class="list-group-item list-group-item-action cursor-active">
-    <div class="row">
-    <div class="col">
-        <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">${infoProducto.name}
-            </h5>
-                <small class="text-muted">${infoProducto.category}</small>
-    </div>
-        <p class="mb-1">${infoProducto.description}</p>
-        </div>
-    </div>
-</div>
+
+    infoProductos += ` 
+       <h3>${infoProducto.name}</h3>
+       <hr>
+      <h5>Precio</h5>
+      <p>${infoProducto.currency} ${infoProducto.cost}</p>
+      <hr>
+      <h5>Descripción</h5>
+      <p>${infoProducto.description}</p>
+      <hr>
+      <h5>Categoría</h5>
+      <p>${infoProducto.category}</p>
+      <hr>
+      <h5>Cantidad de vendidos</h5>
+      <p>${infoProducto.soldCount}</p>
+      <hr>
+      
 `
-    }
+
     document.getElementById("product-info").innerHTML = infoProductos;
 
 }
 
 
-function mostrarComentarios(comments){
-    
+function mostrarComentarios(comments) {
+
     let comentarios = "";
-    for (let i = 0; i < comments.length; i++){
-    let comentario = comments[i];
-    
-    
-    comentarios += `
+    for (let i = 0; i < comments.length; i++) {
+        let comentario = comments[i];
+
+
+        comentarios += `
+
     <div class="list-group-item list-group-item-action cursor-active">
     <div class="row">
     <div class="col">
         <div class="d-flex w-100 justify-content-between">
-            <h5 class="mb-1">${comentario.user} - Puntuación: ${scoreStars()}</h5>
+            
+            <h6 class="mb-1">${comentario.user} - Puntuación: ${scoreStars()}</h6>
                 <small class="text-muted">${comentario.dateTime}</small> 
     </div>
         <p class="mb-1">${comentario.description}</p>
@@ -54,52 +57,48 @@ function mostrarComentarios(comments){
     </div>
 </div>
 
-    ` 
-    function scoreStars(){
-        let n = comentario.score
-        let estrellas = `<span class="fa fa-star checked"></span>`.repeat(n);
-        return estrellas;
-    
+    `
+        function scoreStars() {
+            let n = comentario.score
+            let estrellas = `<span class="fa fa-star checked"></span>`.repeat(n);
+            return estrellas;
+
+        }
+
+
+
+        document.getElementById("comments").innerHTML = comentarios;
+
+
     }
-}
-    document.getElementById("comments").innerHTML = comentarios;
 
 
 }
 
 
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
 
-    getJSONData(PRODUCTS_INFO_URL).then(function(resultado){
-        if (resultado.status == "ok"){
+    getJSONData(PRODUCTS_INFO_URL).then(function (resultado) {
+        if (resultado.status == "ok") {
             productInfo = resultado.data;
             mostrarInfoProd(productInfo);
         } else {
             alert(resultado.data);
-            
+
         }
-        
-        getJSONData(PRODUCTS_COMMENTS).then(function(result){
-            if (result.status == "ok"){
-                comments = result.data;
-                mostrarComentarios(comments);
-            } else {
-                alert(result.data);   
-            }    
-            
-        });
-        
-    });
 
-
-    document.getElementById("buttonEnviar").addEventListener("click", function(){
-
-        if(estrellitas > 5 || estrellitas < 1){
-            alert("Tu puntuación debe ser del 1 al 5")
-        }
 
     });
 
 });
 
+getJSONData(PRODUCTS_COMMENTS).then(function (result) {
+    if (result.status == "ok") {
+        comments = result.data;
+        mostrarComentarios(comments);
+    } else {
+        alert(result.data);
+    }
+
+});
