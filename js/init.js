5e77aca86bfefc02130bb92b4ebe94097094fad0
ferayala250104 <110630323+ferayala250104@users.noreply.github.com>
2022-10-02 23:40:23 -0300

    E-Commerce

--- js/init.js
+++ js/init.js
@@ -6,14 +6,29 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/prod
 const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
-/* Agregado */
+/***** Agregado *****/
 const AUTOS101_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 let getItemID = localStorage.getItem("userID");
 let documento = document.getElementById("userID");
-/*...*/
+/*****...*****/
 
-//gettear en la esquina
+//Mostrar en barra de navegación el usuario o email. Agregué <p> con respectivo id en todos los html para que se visualizara en cada pantalla.
 documento.innerHTML = getItemID;
+
+
+//Redirigir a pantalla de inicio al apretar "Cerrar Sesión"
+document.addEventListener("DOMContentLoaded", function(){
+
+   document.getElementById("close-session").addEventListener("click", function(){
+
+      localStorage.removeItem("userID");
+      window.location = "index.html"
+
+   });
+
+});
+
+
 
 let showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
